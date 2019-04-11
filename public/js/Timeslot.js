9bc0e38c70412ef677633cf0ba0e6fa168e043f4
Maggie <margaretlou2019@u.northwestern.edu>
2019-04-11 11:51:40 -0500

    Timeslot class definition

---
+++ public/js/Timeslot.js
@@ -0,0 +1,12 @@
+function Timeslot(title, startTime, endTime, busyPeople) {
+  this.title = title;
+  this.startTime = startTime;
+  this.endTime = endTime;
+  this.busyPeople = busyPeople;
+}
+
+// Timeslot.prototype.fun = function fun() {
+//
+// };
+  //
+module.exports = Timeslot;
